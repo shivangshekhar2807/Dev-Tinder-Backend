04bacddfb6e8637a3b116af2518a20dc428157e1
shivangshekhar2807 <shivangshekhar302@gmail.com>
2025-07-17 22:47:19 +0530

    get

---
+++ notes.md
@@ -0,0 +1,2 @@
+
+route starts checking from url and tally with the route in the code...if it finds the route and the route is over in the code then it will enter that...but if it tally and the url route is over but still something left in code url then it will search in next one.
--- src/app.js
+++ src/app.js
@@ -2,13 +2,25 @@ const express = require("express")
 
 const app = express();
 
-app.use("/data",(req,res)=> {
-    if (req.url === "data") {
-        res.send("data")
-    }
-    else {
-        res.send("ok serverrrr")
-    }
+
+
+
+app.get("/data",(req,res)=> {
+    
+        res.send("ok serverrrr data")
+    
+})
+
+app.use("/data/2",(req,res)=> {
+    
+    res.send("ok serverrrr data/2")
+
+})
+
+app.use("/hello",(req,res)=> {
+    
+    res.send("ok serverrrr hello")
+
 })
 
 app.listen(3000, () => {
