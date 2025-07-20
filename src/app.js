5903b9574861af007d5ec079331e25ce7c022906
shivangshekhar2807 <shivangshekhar302@gmail.com>
2025-07-20 13:44:12 +0530

    completed API & Schema level Data Sanitization and validation

--- src/app.js
+++ src/app.js
@@ -1,87 +1,73 @@
-// const express = require("express");
-// const app = express();
-// const connectDB = require("./config/database");
-// const UserModel  = require("./models/user");
-
-// connectDB()
-//   .then((res) => {
-//     console.log("database CONNECTED");
-//     app.listen(3000, () => {
-//       console.log("starting backend");
-//     });
-//   })
-//   .catch((err) => {
-//     console.log(err, "database CONNECTION Failed");
-//   });
-
-// app.use(express.json());
-    
-
-// app.post("/signUp", async (req, res) => {
-  
-//   console.log(req.body)
-      
-//     const userObj = {
-//         firstName: "Virat",
-//         lastName: "Kohli",
-//         email: "ss@g.com",
-//         password:"ss621311"
-//     }
-
-//     const user = new UserModel(req.body);
-    
-//    const userRes= await user.save();
-
-//     res.send(`saved Successfully ${userRes}`);
-
-//   })
-
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const connectDB = require('./config/database');
+const connectDB = require("./config/database");
 const UserModel = require("./models/user");
 
 connectDB().then(() => {
   console.log("DataBase CONNNECTED");
   app.listen(3000, () => {
-    console.log("starting backened")
+    console.log("starting backened");
   });
-})
+});
 
-app.use(express.json())
+app.use(express.json());
 
-app.post('/ok', async(req, res, next) => {
-  
-  const user = new UserModel(req.body)
-  
+app.post("/user", async (req, res, next) => {
+  const user = new UserModel(req.body);
+
   const userRes = await user.save();
 
-
   res.send(`saved to DB ${userRes}`);
-})
+});
 
-
-app.get('/user', async (req, res) => {
-
-  const found = await UserModel.find({ firstName: req.body.firstName })
+app.get("/user", async (req, res) => {
+  const found = await UserModel.find({ firstName: req.body.firstName });
 
   if (found.length < 1) {
-    res.status(404).send("User Not Found")
+    res.status(404).send("User Not Found");
   }
-  
-  res.send(`user found ${found}`)
-  
-})
+
+  res.send(`user found ${found}`);
+});
 
 app.delete("/user", async (req, res) => {
   const found = await UserModel.findByIdAndDelete(req.body.userId);
 
- 
-
   res.send(`user deleted ${found}`);
 });
 
+app.patch("/user/:userId", async (req, res) => {
+  const id = req.params?.userId;
+  const data = req.body;
+
+  try {
+    const ALLOWED_UPDATED = [
+      
+      "skills",
+      "age",
+      "gender",
+      "photoUrl",
+      "about",
+      "firstName"
+    ];
+  
+    const isUpdatedAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATED.includes(k)
+    );
+  
+    if (!isUpdatedAllowed) {
+      res.status(400).send("updated not Allowed")
+    }
+    const found = await UserModel.findByIdAndUpdate(id, data, {
+      runValidators: true,
+    });
+  
+    res.send(`user Updated ${found}`);
+  }
+  catch (err) {
+    res.status(400).send("updation failed"+err.message);
+  }
+});
 
 app.get("/Alluser", async (req, res) => {
   const found = await UserModel.find({});
@@ -92,5 +78,3 @@ app.get("/Alluser", async (req, res) => {
 
   res.send(`user found ${found}`);
 });
-
-
--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,4 @@
 
-// const mongoose = require('mongoose');
-
-// const connectDB = async () => {
-//     await mongoose.connect("mongodb+srv://shivangshekhar302:ss621311@shivang-dev.qdfs9rq.mongodb.net/Tinder")
-// }
-
-
-
-// module.exports = connectDB;
-
-
-
-
-
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
